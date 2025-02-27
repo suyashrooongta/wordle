28a220519d181469a61a30db7aa07f19ed687ab6
suyashrooongta <41545593+suyashrooongta@users.noreply.github.com>
2025-02-27 11:13:49 +0530

    Basic board for wordle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board'
+import Keyboard from './components/Keyboard'
+import {createContext , useState } from 'react';
+import { boardDefault } from './components/Words'
+
+export const AppContext = createContext()
 
 function App() {
+  const [board, setBoard] = useState(boardDefault)
   return (
     <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+        <AppContext.Provider value = {{board, setBoard}}>
+        <Board />
+        <Keyboard />
+        </AppContext.Provider>
+      </nav>
     </div>
   );
 }
