4915ee67bdd205733881421f7f58459fcc24e7ed
suyashrooongta <41545593+suyashrooongta@users.noreply.github.com>
2025-02-27 14:44:44 +0530

    "Adding keyboard input"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import Board from './components/Board'
-import Keyboard from './components/Keyboard'
-import {createContext , useState } from 'react';
-import { boardDefault } from './components/Words'
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { createContext, useState } from "react";
+import { boardDefault } from "./components/Words";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 function App() {
-  const [board, setBoard] = useState(boardDefault)
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 })
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
-        <AppContext.Provider value = {{board, setBoard}}>
-        <Board />
-        <Keyboard />
+        <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt }}>
+          <div className="game">
+            <Board />
+            <Keyboard />
+          </div>
         </AppContext.Provider>
       </nav>
     </div>
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
+import Key from "./Key";
 
 function Keyboard() {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
   return (
-    <div className='keyboard'>
-      <div className='line1'></div>
-      <div className='line2'></div>
-      <div className='line3'></div>
+    <div className="keyboard">
+      <div className="line1">
+        {keys1.map((key) => {
+          return <Key keyVal={key} />;
+        })}
+      </div>
+      <div className="line2">
+        {keys2.map((key) => {
+          return <Key keyVal={key} />;
+        })}</div>
+      <div className="line3">
+        <Key keyVal={"ENTER"} bigKey={"True"}/>
+        {keys3.map((key) => {
+          return <Key keyVal={key} />;
+        })}
+        <Key keyVal={"DELETE"} bigKey={"True"}/></div>
     </div>
-  )
+  );
 }
 
-export default Keyboard
+export default Keyboard;
